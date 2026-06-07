78060a6c048373a87f16c9ee94a9fe827be70462
AbdulRahman O. <86073871+RahmanC@users.noreply.github.com>
2026-06-07 08:45:08 +0100

    chore: update assembly informational version and cache inputs

--- obj/Debug/net8.0/EmployeeLeaveManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeLeaveManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeLeaveManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a3e2fbed749acc23392c034926cc638fb966b9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21e97eca2b4db330f3fe22660daee93861b627c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeLeaveManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeLeaveManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
